fc91375fc67cdcf29de2331ceedb3e77eb12b6a1
戴曦 <daix@mingyuanyun.com>
2020-01-02 17:52:51 +0800

    提交

---
+++ src/views/Layout/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './index.less';
+
+const Header = () => {
+  return <div className={classes.header}></div>;
+};
+
+export default Header;
--- src/views/Layout/index.js
+++ src/views/Layout/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '@/views/Home';
+import Header from '@/views/Layout/Header';
+import Sidebar from '@/views/Layout/Sidebar';
+import Content from '@/views/Layout/Content';
 
+import classes from './index.less';
+
+const styleString = `
+html,body,#root {height: 100%}
+#root {
+  display: flex;
+
+}
+`;
 const Layout = () => {
   return (
-    <Switch>
-      <Route path="/home" component={Home} />
-    </Switch>
+    <>
+      <style dangerouslySetInnerHTML={{ __html: styleString }} />
+      <Sidebar className={classes.left} />
+      <div className={classes.right}>
+        <Header />
+        <Content />
+      </div>
+    </>
   );
 };
 
